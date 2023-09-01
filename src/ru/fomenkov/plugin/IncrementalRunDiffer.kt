1377136a4c3261893549e5d6d495d00d9af05f7a
Andrey Fomenkov <andrey.fomenkov@indriver.com>
2023-09-01 14:39:16 +0300

    Incremental run differ.

---
+++ src/ru/fomenkov/plugin/IncrementalRunDiffer.kt
@@ -0,0 +1,100 @@
+package ru.fomenkov.plugin
+
+import ru.fomenkov.utils.Log
+import ru.fomenkov.utils.WorkerTaskExecutor
+import java.io.File
+import java.util.concurrent.Callable
+
+class IncrementalRunDiffer(
+    private val executor: WorkerTaskExecutor,
+    private val intermediateBuildPath: String,
+    private val finalBuildPath: String,
+) {
+    data class Result(
+        val compileSourcePaths: Set<String>,
+        val removeSourcePaths: Set<String>,
+    )
+
+    private val metadataFileName = "metadata.diff"
+
+    /**
+     * Get all dirty source files and return file subsets to compile
+     * and remove based on the previous incremental run
+     */
+    fun run(gitDiffPaths: Set<String>): Result {
+        require(gitDiffPaths.isNotEmpty()) { "No files to process" }
+        check(File(intermediateBuildPath).exists()) { "Path doesn't exist: $intermediateBuildPath" }
+        check(File(finalBuildPath).exists()) { "Path doesn't exist: $finalBuildPath" }
+
+        val prevHashTable = readHashTable()
+        val calcStartTime = System.currentTimeMillis()
+        val currHashTable = computeHashes(gitDiffPaths)
+        val calcEndTime = System.currentTimeMillis()
+
+        Log.v("# Hash tables comparison (calc time = ${calcEndTime - calcStartTime} ms)#")
+
+        (prevHashTable.keys + currHashTable.keys).forEach { path ->
+            val prevHashValue = prevHashTable[path]
+            val currHashValue = currHashTable[path]
+            val mark = when {
+                currHashValue != null && currHashValue != prevHashValue -> "[COMPILE]"
+                currHashValue == null && prevHashValue != null -> "[REMOVE]"
+                else -> ""
+            }
+            Log.v("[HASH] $path: BEFORE(${prevHashValue ?: "---"}) -> AFTER(${currHashValue ?: "---"}) $mark")
+        }
+        Log.v("")
+        writeHashTable(currHashTable)
+        val compileSourcePaths = mutableSetOf<String>()
+        val removeSourcePaths = mutableSetOf<String>()
+
+        (prevHashTable.keys + currHashTable.keys).forEach { path ->
+            val prevHashValue = prevHashTable[path]
+            val currHashValue = currHashTable[path]
+
+            if (currHashValue != null && currHashValue != prevHashValue) {
+                compileSourcePaths += path
+            } else if (currHashValue == null && prevHashValue != null) {
+                removeSourcePaths += path
+            }
+        }
+        return Result(
+            compileSourcePaths = compileSourcePaths,
+            removeSourcePaths = removeSourcePaths,
+        )
+    }
+
+    private fun computeHashes(paths: Set<String>) = paths.map { path ->
+        Callable {
+            val file = File(path)
+            check(file.exists()) { "Source file doesn't exist: $path" }
+            path to file.readText().hashCode()
+        }
+    }
+        .let(executor::run)
+        .associate { (path, hash) -> path to hash }
+
+    /**
+     * Create current diff hashtable
+     * Format: `<path>#<hash>`
+     */
+    private fun writeHashTable(hashTable: Map<String, Int>) {
+        val text = hashTable.entries.joinToString(separator = "\n") { (path, hash) -> "$path#$hash" }
+        File("$finalBuildPath/$metadataFileName").writeText(text)
+    }
+
+    private fun readHashTable(): Map<String, Int> {
+        val file = File("$finalBuildPath/$metadataFileName")
+
+        if (!file.exists()) {
+            return emptyMap()
+        }
+        return file.readLines().associate { line ->
+            val parts = line.split('#')
+            check(parts.size == 2) { "Failed to parse line from hashtable file: $line" }
+            val path = parts[0]
+            val hash = parts[1].toInt()
+            path to hash
+        }
+    }
+}
---
+++ test/ru/fomenkov/plugin/IncrementalRunDifferTest.kt
@@ -0,0 +1,156 @@
+package ru.fomenkov.plugin
+
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import ru.fomenkov.Settings
+import ru.fomenkov.shell.Shell.exec
+import ru.fomenkov.utils.Log
+import ru.fomenkov.utils.WorkerTaskExecutor
+import java.io.File
+import kotlin.random.Random
+
+class IncrementalRunDifferTest {
+
+    private val testRootDirectory = "test_differ"
+    private val intermediateBuildDirectory = "$testRootDirectory/intermediate"
+    private val finalBuildDirectory = "$testRootDirectory/final"
+    private val diffBuildDirectory = "$testRootDirectory/diff"
+
+    private lateinit var executor: WorkerTaskExecutor
+    private lateinit var differ: IncrementalRunDiffer
+
+    @Before
+    fun setup() {
+        Settings.isVerboseMode = true
+        exec("rm -rf $testRootDirectory")
+        assertFalse(File(testRootDirectory).exists())
+
+        exec("mkdir $testRootDirectory")
+        exec("mkdir $intermediateBuildDirectory")
+        exec("mkdir $finalBuildDirectory")
+        exec("mkdir $diffBuildDirectory")
+        assertTrue(File(testRootDirectory).exists())
+        assertTrue(File(intermediateBuildDirectory).exists())
+        assertTrue(File(finalBuildDirectory).exists())
+        assertTrue(File(diffBuildDirectory).exists())
+
+        executor = WorkerTaskExecutor()
+        differ = IncrementalRunDiffer(
+            executor = executor,
+            intermediateBuildPath = intermediateBuildDirectory,
+            finalBuildPath = finalBuildDirectory,
+        )
+    }
+
+    @Test
+    fun `Test incremental diff steps`() {
+        // Step 1: add sources 1, 2, 3
+        assertEquals(
+            IncrementalRunDiffer.Result(
+                compileSourcePaths = setOf(
+                    "$diffBuildDirectory/Source_1.kt".createOrUpdate(),
+                    "$diffBuildDirectory/Source_2.kt".createOrUpdate(),
+                    "$diffBuildDirectory/Source_3.kt".createOrUpdate(),
+                ),
+                removeSourcePaths = emptySet(),
+            ),
+            diff(
+                "$diffBuildDirectory/Source_1.kt".createOrUpdate(),
+                "$diffBuildDirectory/Source_2.kt".createOrUpdate(),
+                "$diffBuildDirectory/Source_3.kt".createOrUpdate(),
+            ),
+        )
+        // Step 2: modify source 2
+        assertEquals(
+            IncrementalRunDiffer.Result(
+                compileSourcePaths = setOf(
+                    "$diffBuildDirectory/Source_2.kt",
+                ),
+                removeSourcePaths = emptySet(),
+            ),
+            diff(
+                "$diffBuildDirectory/Source_1.kt",
+                "$diffBuildDirectory/Source_2.kt".createOrUpdate(),
+                "$diffBuildDirectory/Source_3.kt",
+            )
+        )
+        // Step 3: modify sources 1, 3
+        assertEquals(
+            IncrementalRunDiffer.Result(
+                compileSourcePaths = setOf(
+                    "$diffBuildDirectory/Source_1.kt",
+                    "$diffBuildDirectory/Source_3.kt",
+                ),
+                removeSourcePaths = emptySet(),
+            ),
+            diff(
+                "$diffBuildDirectory/Source_1.kt".createOrUpdate(),
+                "$diffBuildDirectory/Source_2.kt",
+                "$diffBuildDirectory/Source_3.kt".createOrUpdate(),
+            ),
+        )
+        // Step 4: delete source 1 and modify sources 3, 4
+        assertEquals(
+            IncrementalRunDiffer.Result(
+                compileSourcePaths = setOf(
+                    "$diffBuildDirectory/Source_3.kt",
+                    "$diffBuildDirectory/Source_4.kt",
+                ),
+                removeSourcePaths = setOf(
+                    "$diffBuildDirectory/Source_1.kt",
+                ),
+            ),
+            diff(
+                "$diffBuildDirectory/Source_2.kt",
+                "$diffBuildDirectory/Source_3.kt".createOrUpdate(),
+                "$diffBuildDirectory/Source_4.kt".createOrUpdate(),
+            ),
+        )
+        // Step 5: delete all sources and add source 456
+        assertEquals(
+            IncrementalRunDiffer.Result(
+                compileSourcePaths = setOf(
+                    "$diffBuildDirectory/Source_456.kt",
+                ),
+                removeSourcePaths = setOf(
+                    "$diffBuildDirectory/Source_2.kt",
+                    "$diffBuildDirectory/Source_3.kt",
+                    "$diffBuildDirectory/Source_4.kt",
+                ),
+            ),
+            diff(
+                "$diffBuildDirectory/Source_456.kt".createOrUpdate(),
+            ),
+        )
+    }
+
+    @After
+    fun teardown() {
+        executor.release()
+        exec("rm -rf $testRootDirectory")
+        assertFalse(File(testRootDirectory).exists())
+    }
+
+    private fun diff(vararg gitDiffPaths: String): IncrementalRunDiffer.Result {
+        return differ.run(gitDiffPaths.toSet())
+            .also { (compileSourcePaths, removeSourcePaths) ->
+                compileSourcePaths.forEach { path -> Log.v("[COMPILE] $path") }
+                removeSourcePaths.forEach { path -> Log.v("[REMOVE]  $path") }
+                Log.v("")
+            }
+    }
+
+    private fun String.createOrUpdate(): String {
+        val builder = StringBuilder()
+        val linesCount = Random.nextInt(from = 10, until = 30)
+
+        for (i in 0 until linesCount) {
+            builder.append(Random.nextLong())
+            builder.append('\n')
+        }
+        File(this).writeText(builder.toString())
+        return this
+    }
+}
