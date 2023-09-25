4085279a34a5f98b018acbcfc42163f1e855d0f0
Andrey Fomenkov <andrey.fomenkov@indriver.com>
2023-09-25 20:16:28 +0300

    Snapshot test comment.

--- test/ru/fomenkov/plugin/bytecode/ClassFileInjectionSnapshotPlayground.kt
+++ test/ru/fomenkov/plugin/bytecode/ClassFileInjectionSnapshotPlayground.kt
@@ -5,6 +5,51 @@ import org.junit.Test
 import ru.fomenkov.Settings
 import ru.fomenkov.utils.Log
 
+/**
+ * ### Source code for test_data/SampleInjectionClass.class ###
+ *
+ * import dagger.assisted.AssistedInject
+ * import javax.inject.Inject
+ *
+ * class SampleInjectionClass {
+ *
+ *     @Inject
+ *     constructor(arg1: Int) {
+ *     }
+ *
+ *     @AssistedInject
+ *     constructor(arg1: Long, arg2: String) {
+ *     }
+ *
+ *     constructor(arg1: Long, arg2: String, arg3: Byte) {
+ *     }
+ *
+ *     @Inject
+ *     lateinit var lat1: String
+ *
+ *     lateinit var lat2: String
+ *
+ *     val con1 = "ABC-123"
+ *
+ *     @Inject
+ *     fun fooInj(arg1: Int): String {
+ *         return ""
+ *     }
+ *
+ *     @Inject
+ *     fun barInj(arg1: Int, arg2: Long): Double {
+ *         return 0.0
+ *     }
+ *
+ *     fun foo(arg1: Byte): Long {
+ *         return 1
+ *     }
+ *
+ *     fun bar(arg1: Byte, arg2: Char): Short {
+ *         return 2
+ *     }
+ * }
+ */
 class ClassFileInjectionSnapshotPlayground {
 
     @Before
